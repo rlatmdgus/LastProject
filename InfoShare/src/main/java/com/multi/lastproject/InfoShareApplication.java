886df60efbf945462db56a0c779b813a57cc4a95
rlatmdgus <gha0810@naver.com>
2022-02-16 15:50:28 +0900

    메인,로그인,회원가입

--- InfoShare/src/main/java/com/multi/lastproject/InfoShareApplication.java
+++ InfoShare/src/main/java/com/multi/lastproject/InfoShareApplication.java
@@ -1,13 +1,18 @@
 package com.multi.lastproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.multi.lastproject.controller.MainController;
+import com.multi.lastproject.controller.MemberController;
+import com.multi.lastproject.dao.IMemberDAO;
 
 @SpringBootApplication
 @ComponentScan(basePackageClasses = MainController.class)
+@ComponentScan(basePackageClasses = MemberController.class)
+@MapperScan(basePackageClasses = IMemberDAO.class)
 public class InfoShareApplication {
 
 	public static void main(String[] args) {
---
+++ InfoShare/src/main/java/com/multi/lastproject/model/MemberVO.java
@@ -0,0 +1,55 @@
+package com.multi.lastproject.model;
+
+public class MemberVO {
+	
+	private String memId;
+	private String memPw;
+	private String memBirth;
+	private String memName;
+	private String memHp;
+	private String memEmail;
+	private String memAddress;
+	
+	public String getMemId() {
+		return memId;
+	}
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+	public String getMemPw() {
+		return memPw;
+	}
+	public void setMemPw(String memPw) {
+		this.memPw = memPw;
+	}
+	public String getMemBirth() {
+		return memBirth;
+	}
+	public void setMemBirth(String memBirth) {
+		this.memBirth = memBirth;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	public String getMemHp() {
+		return memHp;
+	}
+	public void setMemHp(String memHp) {
+		this.memHp = memHp;
+	}
+	public String getMemEmail() {
+		return memEmail;
+	}
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+	public String getMemAddress() {
+		return memAddress;
+	}
+	public void setMemAddress(String memAddress) {
+		this.memAddress = memAddress;
+	}
+}
